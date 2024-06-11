784dd54c713587c03350faf331982017a1046995
유진 <yujiniii@icloud.com>
2024-06-11 19:33:17 +0900

    feat: HTTP 메서드 변경 및 주석 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
 import {
+  Body,
   Controller,
   Get,
   HttpCode,
   Param,
-  Query,
-  Req,
+  Post,
   Res,
 } from '@nestjs/common';
 import { AppService, ShortUrlType } from './app.service';
@@ -14,16 +14,19 @@ import { Request, Response } from 'express';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/test')
-  getShortUrlTest(@Query('url') url: string): ShortUrlType {
-    return this.appService.getShortUrl(url);
-  }
-
+  /* 단축 URL을 통해 접속하는 API (테스트를 위해 임의 생성) */
   @Get('/real/:key')
   getRealUrl(@Param('key') key: string): string {
     return `welcome! this is real url page! ** ${key} ** `;
   }
 
+  /* 단축 URL을 생성하는 API */
+  @Post('/short')
+  makeShortUrl(@Body() body: { url: string }): ShortUrlType {
+    return this.appService.getShortUrl(body.url);
+  }
+
+  /* 단축 URL로 리다이렉트하는 API */
   @Get('/short/:key')
   @HttpCode(302)
   redirectShortUrl(@Param('key') key: string, @Res() res: Response): void {
